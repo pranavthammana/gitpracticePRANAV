d9d8a7e34acf1756330ec32d5601d4abfef01c15
195-084 <59235137+195-084@users.noreply.github.com>
2020-03-03 15:33:52 -0800

    getNRandom function finished

--- practicePRANAV.py
+++ practicePRANAV.py
@@ -2,17 +2,23 @@
 #Partner2: Vivek Mittal-Henkle
 ##############
 #Assignment name: github practice - 20 points - 3/3/2020
+import random as rand
 
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
-    pass
+	ints = []
+	for i in range(int(n)):
+		ints.append(rand.randint(0, 10))
+	return ints
+print(getNRandom(2))
 
 def multiplyRandom(numbers):
 	'''takes in a list of n numbers and returns the product of the numbers'''
-    pass
+	pass
 
 def main():
-	print(multiplyRandom(getNRandom(10))
+	#print(multiplyRandom(getNRandom(10)))
+	pass
 
 if __name__ == "__main__":
 	main()
